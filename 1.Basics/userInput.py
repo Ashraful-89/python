f1aae77f06deac014647678c5e22917970a4ba19
MD : ASHRAFUL ISLAM <97118095+Ashraful-89@users.noreply.github.com>
2023-09-16 22:52:04 +0600

    Add files via upload

---
+++ 1.Basics/userInput.py
@@ -0,0 +1,7 @@
+user_name = input("Enter your name : ")
+user_age = int(input("Enter user your age : "))
+print("Hello Mr", user_name, "and you are ", user_age," year old.")
+
+user_height = float(input("Enter your age :"))  # take a float number
+print("Your height " ,user_height)
+#input() always take a input from string format
