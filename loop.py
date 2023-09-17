0f9317b49ee5dc25041af6739cc14c582fbb73b7
MD : ASHRAFUL ISLAM <97118095+Ashraful-89@users.noreply.github.com>
2023-09-17 22:15:28 +0600

    Create loop.py

---
+++ loop.py
@@ -0,0 +1,17 @@
+for i in range(100): 
+    print("I love Bangladesh") #10 bar print korbe
+
+a = list(range(12))
+print(a)
+
+b = list(range(0,6))  # i = 0; i < 6 ; i++
+print(b)
+
+c = list(range(0,10,2))   # i = 0, i < 10 ; i+= 2
+print(c)
+
+d = list(range(10,0,-1))
+print(d)
+
+f = list(range(10,-2,-3))
+print(f)
